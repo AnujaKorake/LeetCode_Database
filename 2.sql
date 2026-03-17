0731d95e2e271ad666d75071f3416428fadbf566
unknown <anujakorake@gmail.com>
2026-03-17 17:19:59 +0530

    first commit

---
+++ 2.sql
@@ -0,0 +1,31 @@
+"""
+Q2. Employees Earning More Than Their Managers
+
+
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+
+"""
+
+select e.name as Employee from Employee as e
+join Employee as m
+on e.managerId = m.id
+where e.salary > m.salary;  
