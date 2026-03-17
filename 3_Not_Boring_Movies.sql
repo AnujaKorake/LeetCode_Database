71849766504ef6a33a20e86643965d5a5b17dfa2
unknown <anujakorake@gmail.com>
2026-03-17 17:36:23 +0530

    push

--- 3_Not_Boring_Movies.sql
+++ 3_Not_Boring_Movies.sql
@@ -22,4 +22,6 @@ Output:
 Explanation: 
 We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 """
-
+select * from Cinema
+where id%2 != 0 and description != "boring"
+order by rating DESC;
