d9afa21898b044a42fdfa9daf9c698cba8a09ffd
Anuja Mahadev Korake <147719075+AnujaKorake@users.noreply.github.com>
2026-06-07 15:32:32 +0530

    Time: 3 ms (59.9%), Space: 19.3 MB (77.3%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0001-projection-area-of-3d-shapes/0001-projection-area-of-3d-shapes.py
@@ -0,0 +1,27 @@
+class Solution:
+    def projectionArea(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+
+        top = 0
+        front = 0
+        side = 0
+
+        for i in range(n):
+                row_max = 0
+                col_max = 0
+
+                for j in range(n):
+                    if grid[i][j] > 0:
+                        top += 1
+
+                    row_max = max(row_max, grid[i][j])
+                    col_max = max(col_max, grid[j][i])
+
+                front += row_max
+                side += col_max
+            
+        return top + front + side 
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
