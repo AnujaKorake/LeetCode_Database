88b58d52ac5798a4a7433c8ebf06eada40b251f0
Anuja Mahadev Korake <147719075+AnujaKorake@users.noreply.github.com>
2026-07-01 09:40:48 +0530

    Time: 1101 ms (6.84%), Space: 0B (100%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -2,3 +2,7 @@
 delete p1
 from Person p1
 join Person p2 on p1.email = p2.email and p1.id > p2.id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
